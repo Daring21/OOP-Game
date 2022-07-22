77fc5ee79cc3981737ff48215057c756c66b63f8
Franz788 <amqm0walxq@rambler.ru>
2022-07-22 19:30:59 +0300

    Delete unnecessary variables

--- Core/Helpers/SoundHelper.cs
+++ Core/Helpers/SoundHelper.cs
@@ -6,25 +6,15 @@ public static class SoundHelper
 {
     private static readonly WindowsMediaPlayer MusicPlayer = new();
     private static readonly WindowsMediaPlayer SoundsPlayer = new();
-    private static int _musicVolume = 100;
-    private static int _soundVolume = 100;
     public static int MusicVolume
     {
-        get => _musicVolume;
-        set
-        {
-            MusicPlayer.settings.volume = value;
-            _musicVolume = value;
-        }
+        get => MusicPlayer.settings.volume;
+        set => MusicPlayer.settings.volume = value;
     }
     public static int SoundsVolume
     {
-        get => _soundVolume;
-        set
-        {
-            SoundsPlayer.settings.volume = value;
-            _soundVolume = value;
-        }
+        get => SoundsPlayer.settings.volume;
+        set => SoundsPlayer.settings.volume = value;
     }
 
     public static void PlayMusic(string path)
